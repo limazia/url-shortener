5715d5179bfd4833547bd48551ef261e460a1a57
Acacio de Lima <32038004+limazia@users.noreply.github.com>
2022-03-21 00:12:37 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const flash = require("connect-flash");
 
 const app = express();
 
@@ -15,10 +12,6 @@ const { AppConfig } = require("./config");
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/resources/views");
  
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(flash());
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(routes); 
 app.use((err, req, res, next) => {
